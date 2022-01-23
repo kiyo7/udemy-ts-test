f44a09db65f3ec620a51826b397a422be30cee49
kiyo7 <kiyo324783@icloud.com>
2022-01-23 15:34:26 +0900

    CommentPage作成

--- __tests__/Props.test.tsx
+++ __tests__/Props.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import 'setimmediate';
 
 import Post from '../components/Post';
 import { POST } from '../types/Types';
