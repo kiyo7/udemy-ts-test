c8c529c8b196ef374c34aec678d11ef9a741b1c3
kiyo7 <kiyo324783@icloud.com>
2022-01-27 12:08:31 +0900

    warning fix

--- __tests__/BlogPage.test.tsx
+++ __tests__/BlogPage.test.tsx
@@ -10,9 +10,10 @@ import 'setimmediate';
 initTestHelpers();
 
 const handlers = [
-  rest.get(
-    'https://jsonplaceholder.typicode.com/posts/?_limit=10',
-    (req, res, ctx) => {
+  rest.get('https://jsonplaceholder.typicode.com/posts/', (req, res, ctx) => {
+    const query = req.url.searchParams;
+    const _limit = query.get('_limit');
+    if (_limit === '10') {
       return res(
         ctx.status(200),
         ctx.json([
@@ -31,7 +32,7 @@ const handlers = [
         ])
       );
     }
-  ),
+  }),
 ];
 
 const server = setupServer(...handlers); //サーバーを立てる
